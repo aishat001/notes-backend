bba5b48be9f54b3afd6242156c8d4136d4a46596
aishat001 <waheedaishat@gmail.com>
2021-06-16 18:40:10 +0100

    commit

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
