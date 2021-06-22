6f2f6ae9e5c748832151fc7713f6b4ed09e76a78
aishat001 <waheedaishat@gmail.com>
2021-06-22 14:54:47 +0100

    part4-1 project structure

---
+++ app.js
@@ -0,0 +1,39 @@
+const config = require('./utils/config')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const cors = require('cors')
+const noteRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { 
+    useNewUrlParser: true,
+     useUnifiedTopology: true,
+      useFindAndModify: false,
+       useCreateIndex: true,
+       autoIndex: false,
+       poolSize: 10,
+       bufferMaxEntries: 0,
+       connectTimeoutMS: 0,
+       socketTimeoutMS: 0,
+       family: 4 })
+    .then(() => {
+      logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+      logger.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
+app.use('/api/notes', noteRouter)
+app.use(middleware.requestLogger)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+module.exports = app
